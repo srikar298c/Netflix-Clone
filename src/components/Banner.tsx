0634646be040273f652b0521368c01f6f8065b11
sri-gudakesa2703 <srikar2985969c@gmail.com>
2023-04-11 23:57:28 +0530

    connecting to API

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Movie} from "../../typings";
+import Image from "next/image";
 
-function Banner() {
+interface Props {
+    netflixOriginals: Movie[]
+}
+
+function Banner({netflixOriginals}: Props) {
+    const {movie, setMovie} = useState<Movie | null>(null)
+    useEffect(() => {
+        setMovie(
+            netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+        )
+
+    }, [netflixOriginals])
     return (
-        <div></div>
+        <div>
+            <div><Image src={`${baseUrl}${movie?.backdrop_pat || movie?.poster_path}`} alt={}/></div>
+        </div>
     );
 }
 
