386c92c4ebb71535a5d67bd889267b82674bdeac
sri-gudakesa2703 <srikar2985969c@gmail.com>
2023-04-12 12:56:03 +0530

    completed main Banner.tsx

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,13 +1,14 @@
 import React, {useEffect, useState} from 'react';
 import {Movie} from "../../typings";
 import Image from "next/image";
+import {baseUrl} from "@/constants/movies";
 
 interface Props {
     netflixOriginals: Movie[]
 }
 
 function Banner({netflixOriginals}: Props) {
-    const {movie, setMovie} = useState<Movie | null>(null)
+    const [movie, setMovie] = useState<Movie | null>(null)
     useEffect(() => {
         setMovie(
             netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
@@ -16,7 +17,12 @@ function Banner({netflixOriginals}: Props) {
     }, [netflixOriginals])
     return (
         <div>
-            <div><Image src={`${baseUrl}${movie?.backdrop_pat || movie?.poster_path}`} alt={}/></div>
+            <div className={"absolute top-0 left-0 h-[95vh] w-screen"}>
+                <Image src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+
+                       layout="fill"
+                       objectFit={"cover"}/>
+            </div>
         </div>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,7 +33,7 @@ const Home = ({
                 <title> Home-Netflix</title>
             </Head>
             <Header/>
-            <main>
+            <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16 ">
                 < Banner netflixOriginals={netflixOriginals}/>
                 <section>
                     {/*    Row*/}
